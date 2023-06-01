badde9ef3b00888fe38d612c3d994080563780a9
Yair Motro <yair.motro@gmail.com>
2023-06-01 21:28:58 +0000

    added udocker usage

--- run.test.tourmaline.sh
+++ run.test.tourmaline.sh
@@ -1,7 +1,7 @@
-docker run -v $PWD:/data --rm aomlomics/tourmaline:latest snakemake dada2_pe_denoise
-docker run -v $PWD:/data --rm aomlomics/tourmaline:latest snakemake dada2_pe_taxonomy_unfiltered
-docker run -v $PWD:/data --rm aomlomics/tourmaline:latest snakemake dada2_pe_diversity_unfiltered
-docker run -v $PWD:/data --rm aomlomics/tourmaline:latest snakemake dada2_pe_report_unfiltered
-docker run -v $PWD:/data --rm aomlomics/tourmaline:latest snakemake dada2_pe_taxonomy_filtered
-docker run -v $PWD:/data --rm aomlomics/tourmaline:latest snakemake dada2_pe_diversity_filtered
-docker run -v $PWD:/data --rm aomlomics/tourmaline:latest snakemake dada2_pe_report_filtered
+udocker run -v $PWD:/data --rm aomlomics/tourmaline:latest snakemake dada2_pe_denoise
+udocker run -v $PWD:/data --rm aomlomics/tourmaline:latest snakemake dada2_pe_taxonomy_unfiltered
+udocker run -v $PWD:/data --rm aomlomics/tourmaline:latest snakemake dada2_pe_diversity_unfiltered
+udocker run -v $PWD:/data --rm aomlomics/tourmaline:latest snakemake dada2_pe_report_unfiltered
+#udocker run -v $PWD:/data --rm aomlomics/tourmaline:latest snakemake dada2_pe_taxonomy_filtered
+#udocker run -v $PWD:/data --rm aomlomics/tourmaline:latest snakemake dada2_pe_diversity_filtered
+#udocker run -v $PWD:/data --rm aomlomics/tourmaline:latest snakemake dada2_pe_report_filtered
